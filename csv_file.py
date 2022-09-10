b0ffab0b63da5aad0a23f0816edf729ee8ad978d
Antonio Balmaseda <antonio.balmaseda_blanco@roche.com>
2022-09-10 20:26:25 +0200

    adding method to extract fragments from csv files

--- csv_file.py
+++ csv_file.py
@@ -21,4 +21,13 @@ class CsvFile:
             values_to_be_inserted.insert(0, identifier)
             writer = csv.writer(f)
             writer.writerow(values_to_be_inserted)
-            
+
+    def extract_fragment(self, fragment_name):
+        fragment = ""
+        with open(self.path) as f:
+            getting_row = False
+            content_file = f.read()
+            init = content_file.find(f"CATEGORY: {fragment_name},,,") + len(f"CATEGORY: {fragment_name},,,\n")
+            end = content_file.find("\n,,,\n", init)
+            return content_file[init:end+1]
+
