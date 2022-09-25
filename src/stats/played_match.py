0c08235d1cc2c4de673138308d0d1bbe6b4fe8cd
Antonio Balmaseda <antonio.balmaseda_blanco@roche.com>
2022-09-25 13:10:48 +0200

    making played_match cope with match_element

--- src/stats/played_match.py
+++ src/stats/played_match.py
@@ -1,5 +1,6 @@
 from .tackler import Tackler
 from .penalty import Penalty
+from .match_element import MatchElement
 from csv_file import CsvFile
 import os
 import yaml
@@ -27,39 +28,33 @@ class PlayedMatch:
                 print(exc)
                 return
 
+    def get_name(self, config_name):
+        return self.config[config_name]["name"]
+
     def get_combinations(self, config_name):
         combination_list = []
         for combs in product(*self.config[config_name]["keys"].values()):
             combination_list.append("-".join(combs))
         return combination_list
 
-    def analyze_tackles(self, csv_file):
-        csv_file.extract_fragment_by_name(self.TACKLE)
-        rows = csv_file.get_file_content_by_fragment_name(self.TACKLE)
-        for tackle in rows:
-            if tackle[self.PLAYER] not in self.tacklers.keys():
-                self.tacklers[tackle[self.PLAYER]] = Tackler(self.get_combinations("tackle"))
-            self.tacklers[tackle[self.PLAYER]].add_tackle(tackle[self.QUALITY], tackle[self.ZONE])
 
-    def dump_tackles(self, output_path):
+    def analyze_element(self, csv_file, element_name):
+        elements = {}
+        config_name = self.get_name(element_name)
+        csv_file.extract_fragment_by_name(config_name)
+        rows = csv_file.get_file_content_by_fragment_name(config_name)
+        for row in rows:
+            if row[self.PLAYER] not in elements.keys():
+                elements[row[self.PLAYER]] = MatchElement(
+                    self.get_combinations(element_name)
+                )
+            elements[row[self.PLAYER]].add_element("-".join(row[1:]))
+        return elements
+
+    def dump_elements(self, output_path, elements):
         if os.path.exists(output_path):
             os.remove(output_path)
         csv_writer = CsvFile(output_path, ",")
-        for key, tackler in self.tacklers.items():
-            csv_writer.write_csv_file(key, tackler.get_tackles())
-
-    def analyze_penalties(self, csv_file):
-        csv_file.extract_fragment_by_name(self.PENALTY)
-        rows = csv_file.get_file_content_by_fragment_name(self.PENALTY)
-        for penalty in rows:
-            if penalty[self.PLAYER] not in self.penaltiers.keys():
-                self.penaltiers[penalty[self.PLAYER]] = Penalty(self.get_combinations("penalty"))
-            self.penaltiers[penalty[self.PLAYER]].add_penalty(penalty[self.PENALTY_TYPE], penalty[self.ZONE])
-
-    def dump_penalties(self, output_path):
-        if os.path.exists(output_path):
-            os.remove(output_path)
-        csv_writer = CsvFile(output_path, ",")
-        for key, penaltier in self.penaltiers.items():
-            csv_writer.write_csv_file(key, penaltier.get_penalties())
+        for key, element in elements.items():
+            csv_writer.write_csv_file(key, element.get_elements())
 
