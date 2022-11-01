bbb76ddb6e82dd9e25735c1e2a8611f586e65a66
Antonio Balmaseda <antonio.balmaseda_blanco@roche.com>
2022-11-01 08:59:16 +0100

    parsing arguments

--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 from stats.played_match import PlayedMatch
 from csv_file import CsvFile
+import sys, getopt, os
 
-def main():
-    file_input = CsvFile("example.csv")
+def print_error_and_exit():
+    print("usage: parser.py -i <file.csv>")
+    sys.exit(2)
+
+def main(argv):
+    input_file = ""
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["ifile="])
+    except getopt.GetoptError:
+        print_error_and_exit()
+    if opts == []:
+        print_error_and_exit()
+    for opt, arg in opts:
+        if opt == "-h":
+            print_error_and_exit()
+        if opt in ("-i", "--ifile"):
+            input_file = arg
+
+    file_input = CsvFile(input_file)
     played_match = PlayedMatch()
     tackles = played_match.analyze_element(file_input, "tackle")
     played_match.dump_elements("tackles.csv", tackles)
@@ -11,4 +29,4 @@ def main():
     played_match.dump_elements("penalties.csv", penalties)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
